f3709d82bebca21612793742aa760fa840d380c6
Smyrnov Vlad <vladsimovv@gmail.com>
2025-10-04 12:25:23 +0300

    Add other files for proper work

---
+++ src/main/java/com/example/DistEdu/DistEduApplication.java
@@ -0,0 +1,13 @@
+package com.example.DistEdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistEduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DistEdu/DistEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DistEdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
